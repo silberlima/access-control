5e285b740428abfdb21bcb2703696a167e14a10b
silberlima <43426080+silberlima@users.noreply.github.com>
2023-12-28 11:00:17 -0300

    Integration Tests

---
+++ access-control/src/test/java/com/slmtecnologia/integrationtestes/testcontainers/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.slmtecnologia.integrationtestes.testcontainers.swagger;
+
+import com.slmtecnologia.configs.TestConfigs;
+import com.slmtecnologia.integrationtestes.testcontainers.AbstractIntegrationTest;
+import static io.restassured.RestAssured.given;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+    @Test
+    void showDisplaySwaggerUiPage(){
+        var content =  given()
+                        .basePath("/swagger-ui/index.html")
+                        .port(TestConfigs.SERVER_PORT)
+                        .when()
+                            .get()
+                        .then()
+                            .statusCode(200)
+                        .extract()
+                        .body().asString();
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
